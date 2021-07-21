98402def5420bf7789b539ce0933fca57d7fa4a5
Brandon <70345941@continental.edu.pe>
2021-07-21 17:57:09 -0500

    Interactuando con JavaScript desde HTML

--- figuras.js
+++ figuras.js
@@ -1,5 +1,4 @@
 // Código del cuadrado
-const ladoCuadrado = 5;
 const perimetroCuadrado = (lado) => lado*4;
 const areaCuadrado = (lado) => lado*lado;
 
@@ -17,3 +16,18 @@ const areaCirculo = (radio) => pi*radio*radio;
 // console.log("El radio del Circulo es: " +radioCirculo);
 // console.log("El diametro del Circulo es: " +diametroCirculo);
 // console.groupEnd();
+
+
+function calcularPerimetroCuadrado(){
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+  const perimetro = perimetroCuadrado(value);
+  alert(`El perimetro del cuadrado es: ${perimetro}`);
+}
+
+function calcularAreaCuadrado(){
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+  const area = areaCuadrado(value);
+  alert(`El area del cuadrado es: ${area}`);
+}
