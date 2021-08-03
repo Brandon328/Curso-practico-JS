ad8d451fbcd649abc7070c7780e54a94ad5f2ed0
Brandon <70345941@continental.edu.pe>
2021-08-03 09:36:21 -0500

    Taller #4 terminado

--- js/analisis.js
+++ js/analisis.js
@@ -1,50 +1,92 @@
+let datosAS = [];
 
-// HELPERS
-
-function esPar(numero) {
-  return (numero % 2 === 0);
+function persona (name, salary){
+  this.name=name;
+  this.salary=salary;
 }
 
-function calcularPromedio(lista){
-  const sumaLista = lista.reduce((valorAcumulado=0, valorActual)=>
-  valorAcumulado + valorActual);
+function agregarPersona(){
+  const nombre = document.getElementById("InputDatoASNombre");
+  const salario = document.getElementById("InputDatoASSalario");
+  const nombreValue = nombre.value;
+  const salarioValue = salario.value;
+  const rpta = document.getElementById("rptaAS");
+  const btnCalcular = document.getElementById("calcularAnalisis");
   
-  const promedioLista = sumaLista / lista.length;
-  return promedioLista;
-}
-
-// Calculadora de mediana
-function medianaSalarios(lista){
-  const mitad = parseInt(lista.length/2);
-  if(esPar(lista.length)){
-    const persona1 = lista[mitad-1];
-    const persona2 = lista[mitad];
-    
-    const mediana = calcularPromedio([persona1, persona2]);
-    return mediana;
+  if(nombreValue==="" || salarioValue===""){
+    btnCalcular.style.marginBlockEnd="20px";
+    rpta.style.backgroundColor = "red";
+    rpta.textContent="Rellene todos los campos";
   }
   else{
-    const mediana = lista[mitad];
-    return mediana;
+    const lista = document.getElementById("listaDatosAnalisisSalarios");
+    let nPersona = new persona(nombreValue,parseFloat(salarioValue));
+    datosAS.push(nPersona);
+    lista.insertAdjacentHTML('afterbegin',`<div><p>Nombre: ${nombreValue} <br> Salario: S/. ${salarioValue}</p></div><hr>`);
+    nombre.value="";
+    salario.value="";
+    nombre.focus();
   }
 }
 
-// Mediana General
-const salariosCol = colombia.map((persona)=>persona.salary);
-const salariosColSorted = salariosCol.sort((a,b)=>a-b);
+function analisisSalarios(lista){
+  // Mediana General
+  const salarios = lista.map((persona)=>persona.salary);
+  const salariosSorted = ordenarLista(salarios);
+  const medianaGeneral = calcularMediana(salariosSorted);
+  
+  // Mediana del top 10%
+  const spliceStart = parseInt(salariosSorted.length * 0.9);
+  const spliceCount = salariosSorted.length - spliceStart;
+  const salariosTop10 = salariosSorted.splice(spliceStart, spliceCount);
+  const medianaTop10 = calcularMediana(salariosTop10);
+  
+  
+  const analisis = [medianaGeneral, medianaTop10];
+  return analisis;
+}
 
-const medianaGeneralCol = medianaSalarios(salariosColSorted);
+function mostrarAnalisisSalarios(){
+  const rpta = document.getElementById("rptaAS");
+  const btnCalcular = document.getElementById("calcularAnalisis");
+  if(datosAS.length===0){
+    btnCalcular.style.marginBlockEnd="20px";
+    rpta.style.backgroundColor = "red";
+    rpta.textContent="Primero ingrese datos";
+  }
+  else{
+    const analisis=analisisSalarios(datosAS);
+    btnCalcular.style.marginBlockEnd="80px";
+    rpta.style.backgroundColor="#4EABBF";
+    rpta.innerHTML=`<p>La mediana general es: S/. ${analisis[0]} <br>
+    La mediana del TOP 10% es: S/. ${analisis[1]}</p>`;
+    datosAS=[];
+  }
+}
 
 
-// Mediana del top 10%
-const spliceStart = parseInt(salariosColSorted.length * 0.9);
-const spliceCount = salariosColSorted.length - spliceStart;
+function focusInputAS(){
+  const nombre = document.getElementById("InputDatoASNombre");
+  nombre.focus();
+}
 
-const salariosColTop10 = salariosColSorted.splice(spliceStart, spliceCount);
 
-const medianaTop10 = medianaSalarios(salariosColTop10);
+const nombre = document.getElementById("InputDatoASNombre");
+const salario = document.getElementById("InputDatoASSalario");
 
-console.log({
-  medianaGeneralCol, medianaTop10
+
+nombre.addEventListener("keyup", e=>{
+  // Number 13 is the "Enter" key on the keyboard
+  if(e.keyCode===13){
+    e.preventDefault();
+    salario.focus();
+  }
 });
 
+salario.addEventListener("keyup", e=>{
+  // Number 13 is the "Enter" key on the keyboard
+  if(e.keyCode===13){
+    e.preventDefault();
+    document.getElementById("addDatoAS").click();
+  }
+});
--- js/promedio.js
+++ js/promedio.js
@@ -1,23 +1,142 @@
-// ============================== PROMEDIO
+// ============================== Meadia Aritmética
 function calcularPromedio(lista){
   const sumaLista = lista.reduce((valorAcumulado=0, valorActual)=>
   valorAcumulado + valorActual);
-
+  
   const promedioLista = sumaLista / lista.length;
-  return promedioLista;
+  return promedioLista.toFixed(2);
 }
+
+let datosMA = [];
+
+function agregarDatoMA(){
+  const rpta = document.getElementById("rptaMAritmetica");
+  const dato = document.getElementById("InputDatoMA").value;
+  if(dato===""){
+    rpta.style.backgroundColor = "red";
+    rpta.textContent="Ingrese un dato numérico";
+  }
+  else{
+    rpta.style.backgroundColor="#4EABBF";
+    rpta.textContent="";
+    const lista = document.getElementById("listaDatosMA"); 
+    const numDato = parseFloat(dato);
+    datosMA.push(numDato);
+    lista.innerText=datosMA;
+  }
+}
+
+function mostrarPromedio(){
+  const rpta = document.getElementById("rptaMAritmetica");
+  if(datosMA.length===0){
+    rpta.style.backgroundColor = "red";
+    rpta.textContent="Primero ingrese datos";
+  }
+  else{
+    const mediaAritmetica = calcularPromedio(datosMA);
+    rpta.style.backgroundColor="#4EABBF";
+    rpta.textContent="Media Aritmética: "+mediaAritmetica;
+    datosMA=[];
+  }
+}
+
+
+// =============================== Media Geométrica
+let datosMG=[];
+
+function calcularMediaGeometrica(lista){
+  const producto = lista.reduce((a,b)=>a*b);
+  const mediaGeometrica = producto**(lista.length**-1);
+  return mediaGeometrica.toFixed(2);
+}
+
+function agregarDatoMG(){
+  const rpta = document.getElementById("rptaMGeometrica");
+  const dato = document.getElementById("InputDatoMG").value;
+  if(dato===""){
+    rpta.style.backgroundColor = "red";
+    rpta.textContent="Ingrese un dato numérico";
+  }
+  else{
+    rpta.style.backgroundColor="#4EABBF";
+    rpta.textContent="";
+    const lista = document.getElementById("listaDatosMG"); 
+    const numDato = parseFloat(dato);
+    datosMG.push(numDato);
+    lista.innerText=datosMG;
+  }
+}
+
+function mostrarGeometrica(){
+  const rpta = document.getElementById("rptaMGeometrica");
+  if(datosMG.length===0){
+    rpta.style.backgroundColor = "red";
+    rpta.textContent="Primero ingrese datos";
+  }
+  else{
+    const mediaGeometrica = calcularMediaGeometrica(datosMG);
+    rpta.style.backgroundColor="#4EABBF";
+    rpta.textContent="Media Geométrica: "+mediaGeometrica;
+    datosMG=[];
+  }
+}
+
+
+
+// ========================== Media Armónica
+let datosMAr = [];
+
+function calcularMediaArmonica(lista){
+  lista = lista.map(e => e**-1);
+  const sumatoria = lista.reduce((a,b)=>a+b);
+  const mediaArmonica = lista.length / sumatoria;
+  return mediaArmonica.toFixed(2);
+}
+
+function agregarDatoMAr(){
+  const rpta = document.getElementById("rptaMArmonica");
+  const dato = document.getElementById("InputDatoMAr").value;
+  if(dato===""){
+    rpta.style.backgroundColor = "red";
+    rpta.textContent="Ingrese un dato numérico";
+  }
+  else{
+    rpta.style.backgroundColor="#4EABBF";
+    rpta.textContent="";
+    const lista = document.getElementById("listaDatosMAr"); 
+    const numDato = parseFloat(dato);
+    datosMAr.push(numDato);
+    lista.innerText=datosMAr;
+  }
+}
+
+function mostrarArmonica(){
+  const rpta = document.getElementById("rptaMArmonica");
+  if(datosMAr.length===0){
+    rpta.style.backgroundColor = "red";
+    rpta.textContent="Primero ingrese datos";
+  }
+  else{
+    const mediaArmonica = calcularMediaArmonica(datosMAr);
+    rpta.style.backgroundColor="#4EABBF";
+    rpta.textContent="Media Armónica: "+mediaArmonica;
+    datosMAr=[];
+  }
+}
+
 
 
 // ========================== MEDIANA
 function esPar (numero){
-  if(numero %2===0) return true;
-  else return false;
+  return (numero %2===0);
 }
 
 function ordenarLista (lista){
   const listaOrdenada = lista.sort((a,b)=>a-b);
   return listaOrdenada;
 }
+
+let datosMediana = [];
 
 function calcularMediana(lista){
 
@@ -31,16 +150,49 @@ function calcularMediana(lista){
     const e1 = lista[mitadLista-1];
     const e2 = lista[mitadLista];
     mediana = calcularPromedio([e1,e2]);
-    console.log(mediana);
+    return mediana;
   }
   else{
     mediana = lista[mitadLista];
-    console.log(mediana);
+    return mediana;
+  }
+}
+
+function agregarMediana(){
+  const rpta = document.getElementById("rptaMediana");
+  const dato = document.getElementById("InputDatoMediana").value;
+  if(dato===""){
+    rpta.style.backgroundColor = "red";
+    rpta.textContent="Ingrese un dato numérico";
+  }
+  else{
+    rpta.style.backgroundColor="#4EABBF";
+    rpta.textContent="";
+    const lista = document.getElementById("listaDatosMeadiana"); 
+    const numDato = parseFloat(dato);
+    datosMediana.push(numDato);
+    lista.innerText=datosMediana;
+  }
+}
+
+function mostrarMediana(){
+  const rpta = document.getElementById("rptaMediana");
+  if(datosMediana.length===0){
+    rpta.style.backgroundColor = "red";
+    rpta.textContent="Primero ingrese datos";
+  }
+  else{
+    const mediana = calcularMediana(datosMediana);
+    rpta.style.backgroundColor="#4EABBF";
+    rpta.textContent="Mediana: "+mediana;
+    datosMediana=[];
   }
 }
 
 
+
 // ======================== MODA
+
 
 const calcularModa = function(lista){
   let listaCount = {};
@@ -53,23 +205,39 @@ const calcularModa = function(lista){
   const moda = Object.entries(listaCount).sort((a,b)=>{
     return a[1] - b[1];
   }).pop();
-
-  console.log(moda);
+  
+  return moda;
 }
 
+let datosModa=[];
 
-// ========================== Media Armónica
-function calcularMediaArmonica(lista){
-  lista = lista.map(e => e**-1);
-  const sumatoria = lista.reduce((a,b)=>a+b);
-  const mediaArmonica = lista.length / sumatoria;
-  console.log("MA:" +  mediaArmonica);
+function agregarModa(){
+  const rpta = document.getElementById("rptaModa");
+  const dato = document.getElementById("InputDatoModa").value;
+  if(dato===""){
+    rpta.style.backgroundColor = "red";
+    rpta.textContent="Ingrese un dato numérico";
+  }
+  else{
+    rpta.style.backgroundColor="#4EABBF";
+    rpta.textContent="";
+    const lista = document.getElementById("listaDatosModa"); 
+    const numDato = parseFloat(dato);
+    datosModa.push(numDato);
+    lista.innerText=datosModa;
+  }
 }
 
-
-// =============================== Media Geométrica
-function calcularMediaGeometrica(lista){
-  const producto = lista.reduce((a,b)=>a*b);
-  const mediaGeometrica = producto**(lista.length**-1);
-  console.log("MG: " + mediaGeometrica);
+function mostrarModa(){
+  const rpta = document.getElementById("rptaModa");
+  if(datosModa.length===0){
+    rpta.style.backgroundColor = "red";
+    rpta.textContent="Primero ingrese datos";
+  }
+  else{
+    const moda = calcularModa(datosModa);
+    rpta.style.backgroundColor="#4EABBF";
+    rpta.textContent="Moda: "+moda[0];
+    datosModa=[];
+  }
 }
